e8a4112fcf952c9fc297548f1548e0400ec9eea2
Vyankatesh Nakate <108928757+vyankatesh5549@users.noreply.github.com>
2022-07-10 13:00:42 +0530

    Dbmanager with disconnected data access

---
+++ DbManager.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Genric;
+using System.Data;
+using MySql.Data.MySqlClinet;
+
+namspaces DAL{
+public class DbManager{
+  public static string connstring=@"server=localhost,database=knowitdb";
+  public statis list<employee> getall(){
+  
+  }
+}
+}
