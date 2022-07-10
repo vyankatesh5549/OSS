688dca6a6d241034b8c57f63538f1c10cb0a5e81
vyankatesh5549 <vyankateshnakate123@gmail.com>
2022-07-10 12:15:57 +0530

    Event handling changes added

--- Program.cs
+++ Program.cs
@@ -3,20 +3,55 @@ using System.Collections.Generic;
 
 namespace Banking
 {
+
+    public delegate void AccountOperation();
+    
     public class Account{
         public double balance{get;set;}
         public string name { get; set; }// commet added by vahab maner prn 003
+        public event AccountOperation underBalance;
+        public event AccountOperation overBalance;
         public Account(double amount)
         {
             this.balance=amount;
+            if(this.balance<=5000)
+            {
+                underBalance();
+            }
+            else if(this.balance>=250000)
+            {
+
+            overBalance();
+
+            }
         }
         public void Deposit(double amount)
         {
             this.balance+=amount;
+            if(this.balance<=5000)
+            {
+                underBalance();
+            }
+            else if(this.balance>=250000)
+            {
+
+            overBalance();
+
+            }
         }
         public void Withdrow(double amount)
         {
             this.balance-=amount;
+            if(this.balance<=5000)
+            {
+                underBalance();
+            }
+            else if(this.balance>=250000)
+            {
+
+            overBalance();
+
+            }
         }
     }
     public class program
