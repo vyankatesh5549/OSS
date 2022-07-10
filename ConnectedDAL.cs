4e70dbdbddf289e90a41a2976f91c3010c1be0b5
Vyankatesh Nakate <108928757+vyankatesh5549@users.noreply.github.com>
2022-07-10 12:54:12 +0530

    connected data access changes
    connection string

---
+++ ConnectedDAL.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collection.Genric;
+using System.Data.MySqlClinet;
+using System.Data;
+using BOL;
+
+namespaces DAL{
+  public class DbManager{
+  public static string constring=@"";
+}
+}
